c3c761d83b2e403970ed1d64bda532aab234f6bb
James Birtles <jameshbirtles@gmail.com>
2018-02-26 20:25:32 +0000

    Fix build

--- index.ts
+++
@@ -1,5 +0,0 @@
-export * from './src/plugin';
-export * from './src/handler';
-export * from './src/provider';
-export * from './src/providers';
-export * from './src/profile';
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export * from './plugin';
+export * from './handler';
+export * from './provider';
+export * from './providers';
+export * from './profile';
