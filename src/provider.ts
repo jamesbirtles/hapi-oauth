1d667cca8374292eb30e66098fbd604a57bfb473
Vinny <vdog8978@gmail.com>
2020-02-19 14:20:27 +0000

    Microsoft OAuth (#8)
    * Feat/microsoft
    
    * Feat/microsoft remove avatar request
    
    * Feat/microsoft Remove unused avatarUrl
    
    * Feat/microsoft set payload type as a ParsedUrlQueryInput

--- src/provider.ts
+++ src/provider.ts
@@ -1,7 +1,7 @@
-import * as Hapi from 'hapi';
-import * as qs from 'querystring';
 import * as Boom from 'boom';
+import * as Hapi from 'hapi';
 import fetch from 'node-fetch';
+import * as qs from 'querystring';
 
 import { PluginOptions } from './plugin';
 import { Profile } from './profile';
@@ -94,7 +94,7 @@ export abstract class Provider {
                 body = JSON.stringify(payload);
                 break;
             case 'application/x-www-form-urlencoded':
-                body = qs.stringify(payload);
+                body = qs.stringify(payload as qs.ParsedUrlQueryInput);
                 break;
             default:
                 throw new Error(`Unknown encoding type: ${this.encoding}`);
